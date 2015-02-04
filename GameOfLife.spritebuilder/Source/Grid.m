f9958c373a47e50456423bffc8d0aa0cfcb818e4
LollipopLollipop <jodie.d.zhao@gmail.com>
2015-02-03 21:28:55 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -21,7 +21,6 @@ static const int GRID_COLUMNS = 10;
 
 - (void)onEnter
 {
-    //The onEnter method is also the right method to activate touch handling on the grid.
     [super onEnter];
     
     [self setupGrid];
@@ -58,7 +57,7 @@ static const int GRID_COLUMNS = 10;
             _gridArray[i][j] = creature;
             
             // make creatures visible to test this method, remove this once we know we have filled the grid properly
-            //creature.isAlive = YES;
+            creature.isAlive = YES;
             
             x+=_cellWidth;
         }
@@ -66,28 +65,5 @@ static const int GRID_COLUMNS = 10;
         y += _cellHeight;
     }
 }
-
-- (void)touchBegan:(CCTouch *)touch withEvent:(UIEvent *)event
-{
-    //get the x,y coordinates of the touch
-    //CGPoint touchLocation = [touch locationInView: self];
-    CGPoint touchLocation = [touch locationInNode:self];
-    
-    //get the Creature at that location
-    Creature *creature = [self creatureForTouchPosition:touchLocation];
-    
-    //invert it's state - kill it if it's alive, bring it to life if it's dead.
-    creature.isAlive = !creature.isAlive;
-}
-
-- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
-{
-    ////get the row and column that was touched, return the Creature inside the corresponding cell
-    int row = touchPosition.y/_cellHeight;
-    int column = touchPosition.x/_cellWidth;
-    
-    return _gridArray[row][column];
-}
-
 
 @end
